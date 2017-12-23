ad1999dd64b1aea6d018e2a9042cbfabada132b0
Tejasvi <tejpant@gmail.com>
2017-12-23 20:22:06 +0545

    object oriented-line,circle,ellipse simple

---
+++ HGL.cpp
@@ -0,0 +1,153 @@
+extern "C"
+#include <cmath>
+#include "HGL.h"
+//#include"Color.h"
+
+line::line(float x1,float y1,float x2,float y2,Color color):x1(x1),y1(y1),x2(x2),y2(y2),color(color)
+{
+
+  //color=colorz;
+  float dx=x2-x1;
+  float dy=y2-y1;
+  float steps,xinr,yinr;
+  if (dx==0)
+  {
+    steps=fabs(dy);
+    yinr=dy/steps;
+    for (size_t i = 0; i < steps; i++)
+      {
+        point (x1,y1,color);
+        y1+=yinr;
+      }
+  }
+  else if (dy==0)
+  {
+    steps=fabs(dx);
+    xinr=dx/steps;
+    for (size_t i = 0; i < steps; i++)
+      {
+        point (x1,y1,color);
+        x1+=xinr;
+      }
+  }
+  else
+  {
+    if (fabs(dx)>fabs(dy))
+        steps=fabs(dx);
+      else
+        steps=fabs(dy);
+
+      xinr=dx/steps;
+      yinr=dy/steps;
+
+      for (size_t i = 0; i < steps; i++)
+        {
+          point (x1,y1,color);
+          x1=x1+xinr;
+          y1=y1+yinr;
+        }
+  }
+
+    //glFlush();
+
+}
+
+void HGL_init(int argc, char **argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Graphics Project");
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0, 500, 0, 500);
+}
+
+void HGL_End()
+{
+  glFlush();
+	glutMainLoop();
+}
+
+void point(float x, float y, Color color) {
+	glBegin(GL_POINTS);
+  glColor3f(color.r, color.g, color.b);
+	glVertex2f(x, y);
+
+	glEnd();
+}
+
+
+
+
+
+circle::circle(float xc,float yc,float r,Color color):xc(xc),yc(yc),color(color)
+{
+  float x=0, y=r;
+  float p=1-r;
+  while (x<=y)
+  {
+    point(x+xc,y+yc,color);
+    point(-x+xc,-y+yc,color);
+    point(-x+xc,y+yc,color);
+    point(x+xc,-y+yc,color);
+    point(y+xc,x+yc,color);
+    point(-y+xc,-x+yc,color);
+    point(-y+xc,x+yc,color);
+    point(y+xc,-x+yc,color);
+    x++;
+    if (p<0)
+    p+=2*x+1;
+    else
+    {
+      y-=1;
+      p+=2*x+1-2*y;
+    }
+  }
+}
+
+ellipse::ellipse(float xc,float yc,float rx,float ry, Color color):xc(xc),yc(yc),rx(rx),ry(ry),color(color)
+{
+  //region 1
+  float x=0, y=ry;
+  float p=ry*ry-rx*rx*ry+(1/4.0)*rx*rx;
+
+  while (2*ry*ry*x<=2*rx*rx*y)
+  {
+    point(x+xc,y+yc,color);
+    point(-x+xc,-y+yc,color);
+    point(-x+xc,y+yc,color);
+    point(x+xc,-y+yc,color);
+    x++;
+    if (p<0)
+    p+=2*ry*ry*x+ry*ry;
+    else
+    {
+      y-=1;
+      p+=2*ry*ry*x-2*rx*rx*y+ry*ry;
+    }
+  }
+  //region 2
+  p=ry*ry*(x+1/2.0)*(x+1/2.0)+rx*rx*(y-1)*(y-1)-rx*rx*ry*ry;
+  while (y>=0)
+  {
+    point(x+xc,y+yc,color);
+    point(-x+xc,-y+yc,color);
+    point(-x+xc,y+yc,color);
+    point(x+xc,-y+yc,color);
+    y--;
+    if (p<=0)
+    {
+      x++;
+
+      p+=2*ry*ry*x-2*rx*rx*y+rx*rx;
+    }
+    else
+    {
+      y-=1;
+      p-=2*rx*rx*y+rx*rx;
+    }
+  }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #include"HGL.h"
+//#include"Color.h"
+
+class h
+{
+  int a;
+public:
+  h(int a)
+  {
+
+  }
+};
 
 int main(int argc, char **argv) {
   // float x1, y1, x2, y2;
@@ -8,13 +19,15 @@ int main(int argc, char **argv) {
   HGL_init(argc,argv);
 
  line(300,300,400,400,WHITE);
-  line(300,300,400,200,BLUE);
-  line(300,300,200,400,GREEN);
+  line l2(300,300,400,200,BLUE);
+  line l3(300,300,200,400,GREEN);
   line(300,300,200,200,RED);
   line(300,300,300,400,WHITE);
   line(300,300,300,200,BLUE);
   line(300,300,200,300,GREEN);
   line(300,300,400,300,RED);
+  //  line(300,300,400,400,BLUE);
+  h(5);
   circle(100,100,50);
   ellipse(100,100,50,100);
   ellipse(100,100,100,50);
